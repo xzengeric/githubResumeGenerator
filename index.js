f73dfe0b29a66b582a5b4076ee449dfd2ed94657
Eric Zeng <38875467+xzengeric@users.noreply.github.com>
2020-02-13 02:36:25 -0800

    done

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const fs = require("fs"),
   convertFactory = require('electron-html-to');
 const util = require("util");
 const axios = require("axios");
+const generateHTML = require("./generateHTML");
+const Logger = require("./logger");
+const log = new Logger();
 
 
 const answersarry = [];
@@ -14,19 +17,21 @@ function promptUser() {
     {
       type: "input",
       name: "username",
-      message: "What is your name?"
+      message: "What is your github username?"
     },
     {
-      type: "input",
+      type: "list",
       name: "color",
-      message: "What is your favorite color?"
+      message: "What is your favourite color ?",
+      choices: ['green', 'blue','pink','red'],
     },
   ]);
 }
 
 
 async function init() {
-  console.log("hi")
+  log.yellow("Welcome to my Developer Profile Generator !!")
+  log.yellow("let's do it !!")
   try {
 
     const answers = promptUser();
@@ -45,10 +50,7 @@ async function init() {
         var blog = res.data.blog;
         var following = res.data.following;
         var name = res.data.name;
-
-        // console.log(name)
-        // console.log(res)
-
+        var bio = res.data.bio;
 
         answersarry.push({
           'name': name, 'avatarUrl': avatarUrl, 'location': location, 'company': company,
@@ -56,9 +58,9 @@ async function init() {
           'followers': followers,
           'blog': blog,
           'following': following,
-          'color': color
+          'color': color,
+          'bio': bio
         })
-        // console.log(answersarry)
 
         var html = generateHTML(answersarry);
         var conversion = convertFactory({
@@ -69,216 +71,20 @@ async function init() {
           if (err) {
             return console.error(err);
           }
-
-          console.log(result.numberOfPages);
-          console.log(result.logs);
-          result.stream.pipe(fs.createWriteStream('./anywhere.pdf'));
+          
+          result.stream.pipe(fs.createWriteStream(`./${name}-resume.pdf`));
           conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+          log.green(`Operation is successfully completed, Your PDF resume is saved! Good luck!!!!!!!!`);
+          log.white("------------------------------End--------------------------------------");
         });
       })
-
-
-      console.log("---------------------------------------------------------------------------------End");
-
     });
-
+    
   }
   catch (err) {
     console.log(err);
   }
 
 }
-
-
-
-const colors = {
-  green: {
-    wrapperBackground: "#E6E1C3",
-    headerBackground: "#C1C72C",
-    headerColor: "black",
-    photoBorderColor: "#black"
-  },
-  blue: {
-    wrapperBackground: "#5F64D3",
-    headerBackground: "#26175A",
-    headerColor: "white",
-    photoBorderColor: "#73448C"
-  },
-  pink: {
-    wrapperBackground: "#879CDF",
-    headerBackground: "#FF8374",
-    headerColor: "white",
-    photoBorderColor: "#FEE24C"
-  },
-  red: {
-    wrapperBackground: "#DE9967",
-    headerBackground: "#870603",
-    headerColor: "white",
-    photoBorderColor: "white"
-  }
-};
-
-function generateHTML(data) {
-  console.log(data);
-  var data= data[0];
-
-  console.log(data.color);
-  console.log(`${colors[data.color].wrapperBackground}`);
-  console.log(`${data.name}`);
-  return `<!DOCTYPE html>
-  <html lang="en">
-     <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-        <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
-        <title>Document</title>
-        <style>
-            @page {
-              margin: 0;
-            }
-           *,
-           *::after,
-           *::before {
-           box-sizing: border-box;
-           }
-           html, body {
-           padding: 0;
-           margin: 0;
-           }
-           html, body, .wrapper {
-           height: 100%;
-           }
-           .wrapper {
-           background-color: ${colors[data.color].wrapperBackground};
-           padding-top: 100px;
-           }
-           body {
-           background-color: white;
-           -webkit-print-color-adjust: exact !important;
-           font-family: 'Cabin', sans-serif;
-           }
-           main {
-           background-color: #E9EDEE;
-           height: auto;
-           padding-top: 30px;
-           }
-           h1, h2, h3, h4, h5, h6 {
-           font-family: 'BioRhyme', serif;
-           margin: 0;
-           }
-           h1 {
-           font-size: 3em;
-           }
-           h2 {
-           font-size: 2.5em;
-           }
-           h3 {
-           font-size: 2em;
-           }
-           h4 {
-           font-size: 1.5em;
-           }
-           h5 {
-           font-size: 1.3em;
-           }
-           h6 {
-           font-size: 1.2em;
-           }
-           .photo-header {
-           position: relative;
-           margin: 0 auto;
-           margin-bottom: -50px;
-           display: flex;
-           justify-content: center;
-           flex-wrap: wrap;
-           background-color: ${colors[data.color].headerBackground};
-           color: ${colors[data.color].headerColor};
-           padding: 10px;
-           width: 95%;
-           border-radius: 6px;
-           }
-           .photo-header img {
-           width: 250px;
-           height: 250px;
-           border-radius: 50%;
-           object-fit: cover;
-           margin-top: -75px;
-           border: 6px solid ${colors[data.color].photoBorderColor};
-           box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-           }
-           .photo-header h1, .photo-header h2 {
-           width: 100%;
-           text-align: center;
-           }
-           .photo-header h1 {
-           margin-top: 10px;
-           }
-           .links-nav {
-           width: 100%;
-           text-align: center;
-           padding: 20px 0;
-           font-size: 1.1em;
-           }
-           .nav-link {
-           display: inline-block;
-           margin: 5px 10px;
-           }
-           .workExp-date {
-           font-style: italic;
-           font-size: .7em;
-           text-align: right;
-           margin-top: 10px;
-           }
-           .container {
-           padding: 50px;
-           padding-left: 100px;
-           padding-right: 100px;
-           }
-
-           .row {
-             display: flex;
-             flex-wrap: wrap;
-             justify-content: space-between;
-             margin-top: 20px;
-             margin-bottom: 20px;
-           }
-
-           .card {
-             padding: 20px;
-             border-radius: 6px;
-             background-color: ${colors[data.color].headerBackground};
-             color: ${colors[data.color].headerColor};
-             margin: 20px;
-           }
-
-           .col {
-           flex: 1;
-           text-align: center;
-           }
-
-           a, a:hover {
-           text-decoration: none;
-           color: inherit;
-           font-weight: bold;
-           }
-
-           @media print { 
-            body { 
-              zoom: .75; 
-            } 
-           }
-
-        </style>
-        <body>
-        <h1> ${data.name} </h1>
-        </body>
-        
-        
-        
-        `
-}
-
 
 init();
